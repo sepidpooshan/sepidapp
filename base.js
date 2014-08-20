9569717016fa2cef20c38ee1b5e42673d5341cf0
Sepidpooshan.com <info@sepidpooshan.com>
2014-08-20 12:14:45 +0330

    48

--- base.js
+++ base.js
@@ -5,8 +5,21 @@ var nextpage = "#home";
 function onDeviceReady() {
 
 	localStorage.baseUrl = 'http://www.sepidpooshan.com';
-	localStorage.view = 'offsapp';
-	localStorage.display = 'page';
+
+	localStorage.view1 = 'contenttable';
+	localStorage.display1 = 'page';
+
+	localStorage.view2 = 'searchview';
+	localStorage.display2 = 'page';
+
+	localStorage.view3 = 'contenttablesignup';
+	localStorage.display3 = 'page';
+
+	localStorage.view4 = 'offsapp';
+	localStorage.display4 = 'page';
+
+	localStorage.view5 = 'offsapp';
+	localStorage.display5 = 'page';
 
 	username = localStorage.phonegapName;
 	title = localStorage.phonegapTitle;
@@ -14,9 +27,21 @@ function onDeviceReady() {
 	blat = localStorage.phonegapLat;
 	blong = localStorage.phonegapLong;
 	baseurl = localStorage.baseUrl;
-	view = localStorage.view;
-	display = localStorage.display;
 	
+	view1 = localStorage.view1;
+	display1 = localStorage.display1;
+	
+	view2 = localStorage.view2;
+	display2 = localStorage.display2;
+
+	view3 = localStorage.view3;
+	display3 = localStorage.display3;
+
+	view4 = localStorage.view4;
+	display4 = localStorage.display4;
+
+	view5 = localStorage.view5;
+	display5 = localStorage.display5;
 
 	if (baseurl) {
 		$("#baseurl").val(baseurl);
@@ -33,13 +58,46 @@ function onDeviceReady() {
 		$("#story").val(story);
 	};
 
-	if (view) {
-		$("#view").val(view);
+	if (view1) {
+		$("#view1").val(view1);
 	};
 
-	if (display) {
-		$("#display").val(display);
+	if (display1) {
+		$("#display1").val(display1);
 	};
+	
+	if (view2) {
+		$("#view2").val(view2);
+	};
+
+	if (display2) {
+		$("#display2").val(display2);
+	};
+	
+	if (view3) {
+		$("#view3").val(view3);
+	};
+
+	if (display3) {
+		$("#display3").val(display3);
+	};
+
+	if (view4) {
+		$("#view4").val(view4);
+	};
+
+	if (display4) {
+		$("#display4").val(display4);
+	};
+
+	if (view5) {
+		$("#view5").val(view5);
+	};
+
+	if (display5) {
+		$("#display5").val(display5);
+	};
+
 
 	if (localStorage.localLogin == 3) {
 		$("#logoutli").show();
@@ -56,8 +114,16 @@ function onDeviceReady() {
 function saveSettings() {
 	baseurl = $("#baseurl").val();
 	localStorage.baseUrl = baseurl;
-	localStorage.view = $("#view").val();
-	localStorage.display = $("#display").val();
+	localStorage.view1 = $("#view1").val();
+	localStorage.display1 = $("#display1").val();
+	localStorage.view2 = $("#view2").val();
+	localStorage.display2 = $("#display2").val();
+	localStorage.view3 = $("#view3").val();
+	localStorage.display3 = $("#display3").val();
+	localStorage.view4 = $("#view4").val();
+	localStorage.display4 = $("#display4").val();
+	localStorage.view5 = $("#view5").val();
+	localStorage.display5 = $("#display5").val();
 	alert("settings saved");
 }
 
@@ -69,14 +135,86 @@ function loginout() {
 	}
 }
 
-function listArticles() {
+function listArticles1() {
 	url = baseurl + '/phonegap/display_view';
 	$username = '';
-	$("#latestlist").html("retrieving data. you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point ");
+	$("#latestlist").html("در حال پردازش و آماده سازی داده ها");
 	$.post(url, {
 		username : $username,
-		view : localStorage.view,
-		display : localStorage.display
+		view1 : localStorage.view1,
+		display1 : localStorage.display1
+	}, function(data) {
+		if (data == '') {
+			data = "you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point "
+		};
+		$("#latestlist").html(data);
+
+	});
+	$.mobile.changePage("#stories");
+}
+
+function listArticles2() {
+	url = baseurl + '/phonegap/display_view';
+	$username = '';
+	$("#latestlist").html("در حال پردازش و آماده سازی داده ها");
+	$.post(url, {
+		username : $username,
+		view2 : localStorage.view2,
+		display2 : localStorage.display2
+	}, function(data) {
+		if (data == '') {
+			data = "you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point "
+		};
+		$("#latestlist").html(data);
+
+	});
+	$.mobile.changePage("#stories");
+}
+
+function listArticles3() {
+	url = baseurl + '/phonegap/display_view';
+	$username = '';
+	$("#latestlist").html("در حال پردازش و آماده سازی داده ها");
+	$.post(url, {
+		username : $username,
+		view3 : localStorage.view3,
+		display3 : localStorage.display3
+	}, function(data) {
+		if (data == '') {
+			data = "you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point "
+		};
+		$("#latestlist").html(data);
+
+	});
+	$.mobile.changePage("#stories");
+}
+
+function listArticles4() {
+	url = baseurl + '/phonegap/display_view';
+	$username = '';
+	$("#latestlist").html("در حال پردازش و آماده سازی داده ها");
+	$.post(url, {
+		username : $username,
+		view4 : localStorage.view4,
+		display4 : localStorage.display4
+	}, function(data) {
+		if (data == '') {
+			data = "you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point "
+		};
+		$("#latestlist").html(data);
+
+	});
+	$.mobile.changePage("#stories");
+}
+
+function listArticles5() {
+	url = baseurl + '/phonegap/display_view';
+	$username = '';
+	$("#latestlist").html("در حال پردازش و آماده سازی داده ها ");
+	$.post(url, {
+		username : $username,
+		view5 : localStorage.view5,
+		display5 : localStorage.display5
 	}, function(data) {
 		if (data == '') {
 			data = "you must <br/><br/>enable the view in drupal <br/><br/>set the mobile settings page (below)<br/><br/>enable the default views frontage is good starting point "
